e963e97449df97f43054ae2745f17288b9dc9cad
Anonymous4DoubleBlinded <anonymous4doubleblinded@pleasedontreplyforrealthisisnotavalidemail.com>
2024-02-12 09:08:26 -0500

    -

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,11 @@
 rm -rf docs
 cp -rv mkdocs docs
 mkdocs build
+# at this point the github pages content is in a directory called "site"
+rm -rf docs
+mv site docs
+# Now we have what we want: the html / github pages sources in docs, the mkdocs markdowns in mkdocs
 git add .
 git commit -m "-"
 git push
-rm -rf docs
-mv site docs
 
