2e25b6659a06f1c11e4776b316ca2071d3ecc18e
Anonymous4DoubleBlinded <anonymous4doubleblinded@pleasedontreplyforrealthisisnotavalidemail.com>
2024-02-12 09:05:59 -0500

    -

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
+# we need the BUILD site in docs, but mkdoc uses it for its sources usually.
+# So we store the actual sources in mkdocs, then before building override docs, then rename the outcome to docs.
 rm -rf docs
 cp -rv mkdocs docs
 mkdocs build
 git add .
 git commit -m "-"
+git push
 rm -rf docs
 mv site docs
 
